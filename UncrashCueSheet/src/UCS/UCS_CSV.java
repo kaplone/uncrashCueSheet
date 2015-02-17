89fa6e5a39aec5d1b43e3b522e171367d8616e23
kaplone <dvd.brtrnd@gmail.com>
2015-02-17 18:17:25 +0100

    nombreuse corrections

--- UncrashCueSheet/src/UCS/UCS_CSV.java
+++ UncrashCueSheet/src/UCS/UCS_CSV.java
@@ -25,8 +25,12 @@ public class UCS_CSV {
 	private static String ref; // condition : debute par ANW
 	private static String ref_int;
 	
+	private static String name;
+	
 	private static String ref_title_push;
 	private static String ref_title_push_save;
+	
+	private static String ref_title_to_compare;
 	
 	private static HttpClient client;
 	private static HttpGet request ;
@@ -80,18 +84,18 @@ public class UCS_CSV {
 			File f = new File (args[0]);
 			
 			if(f.isDirectory()){
-				System.out.println(args[0] + " est un répertoire, analyse de son contenu ...");
+				System.out.println("\n\n" + args[0] + " est un répertoire, analyse de son contenu ...");
 				for(File f_temp : f.listFiles() ){
 					if (f_temp.isFile() && f_temp.toString().endsWith("edl")){
-						System.out.println("conversion de " + f_temp);
+						System.out.println("\n--------------- conversion de " + f_temp + " ---------------\n" );
 					    convertir(f_temp); 
 					}
 				}	
 			}
 			else{
-				System.out.println(args[0] + " est un fichier ...");
+				System.out.println("\n\n" + args[0] + " est un fichier ...");
 				if (f.toString().endsWith("edl")){
-					System.out.println("conversion de " + f);
+					System.out.println("\n--------------- conversion de " + f  + " ---------------\n" );
 					
 					convertir(f);
 				}
@@ -103,8 +107,19 @@ public class UCS_CSV {
 		}
 	}
 	
-	public static void convertir(File f_edl){
+	protected static String [] getTemps(int seek){
 		
+		String t0 = currentLine.substring(seek).split(" ")[0];
+		//System.out.println(t0);
+		String t1 = currentLine.substring(seek).split(" ")[1];
+		//System.out.println(t1);
+		
+		return new String [] {t0, t1};
+		
+		
+	}
+	
+	public static void convertir(File f_edl){
 		
 		try {
 			
@@ -118,13 +133,17 @@ public class UCS_CSV {
             	
             	if(currentLine.startsWith("TITLE")){
             		elems =  currentLine.split(":")[1].split("_");
-            		bw.write(String.format(intro, elems[3], elems[2], elems[0], elems[1]));
+            		
+            		name = Arrays.asList(elems).subList(3, elems.length	).stream().collect(Collectors.joining("_"));
+            		
+            		
+            		bw.write(String.format(intro, name, elems[2], elems[0], elems[1]));
             		
             	}
             	
-                if(currentLine.contains("FROM CLIP NAME: ANW")){
+                if(currentLine.contains("FROM CLIP NAME: ANW") || currentLine.contains("FROM CLIP NAME:  ANW")){
                 	
-                	entry = currentLine.split("FROM CLIP NAME: ")[1];
+                	entry = currentLine.split("FROM CLIP NAME: ")[1].trim();
                 	
 					ref = String.format("%s/%s", entry.split("_")[0], entry.split("_")[1]);
 					ref_int = String.format("%s/%d", entry.split("_")[0], Integer.parseInt(entry.split("_")[1]));
@@ -132,9 +151,20 @@ public class UCS_CSV {
 					ref_title_push_save = ref_title_push;
 					
 					ref_base = ref.split("/")[0];
+					
+					
+					
+					if ( ! ref_title_push.equals(ref_title_to_compare)){
+						
+						System.out.println("\n(nouveau) titre  : " + ref_title_push);
+			        	
+			        	ref_title_to_compare = ref_title_push;
 
-					surfer(ref_title_push);
-					surfer2();
+					    surfer(ref_title_push);
+					    surfer2();
+					}else {
+						System.out.println("titre  : " + ref_title_push + " (répétition)");
+					}
 					
 					bw.write(String.format("\"%s\",\"%s\",\"%s\",\"\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"\"\n\n",
 							               ref_title_push_save,
@@ -148,51 +178,62 @@ public class UCS_CSV {
 							               ));
                 }
 				
-				if(currentLine.contains("C        ")){
+				if(currentLine.contains("C        ") || currentLine.contains("D    025 ")){
 					
-					String t0 = currentLine.substring(57).split(" ")[0];
-					String t1 = currentLine.substring(57).split(" ")[1];
-					
-					int t0_h = Integer.parseInt(t0.split(":")[0]);
-					int t0_m = Integer.parseInt(t0.split(":")[1]);
-					int t0_s = Integer.parseInt(t0.split(":")[2]);
-					int t0_f = Integer.parseInt(t0.split(":")[3]);
-					int t1_h = Integer.parseInt(t1.split(":")[0]);
-					int t1_m = Integer.parseInt(t1.split(":")[1]);
-					int t1_s = Integer.parseInt(t1.split(":")[2]);
-					int t1_f = Integer.parseInt(t1.split(":")[3]);
+					String t0 = null;
+					String t1 = null;
 					
 					
-					int r_s;
-					int r_m;
-					int r_h;
-					
-					t0_s = t0_f > 15 ? t0_s + 1 : t0_s;
-					t1_s = t1_f > 15 ? t1_s + 1 : t1_s;
-					
-					if (t0_s <= t1_s) {
-						r_s = t1_s - t0_s;
-					}
-					else {
-						r_s = t1_s + 60 - t0_s;
-						t0_m ++;
+					t0 = getTemps(57)[0];
+					t1 = getTemps(57)[1];
+					if (t0.length() != 11){
+						t0 = getTemps(53)[0];
+
 					}
 					
-					if (t0_m <= t1_m) {
-						r_m = t1_m - t0_m;
+					if (t0 != null && t1 != null){
+						
+						int t0_h = Integer.parseInt(t0.split(":")[0]);
+						int t0_m = Integer.parseInt(t0.split(":")[1]);
+						int t0_s = Integer.parseInt(t0.split(":")[2]);
+						int t0_f = Integer.parseInt(t0.split(":")[3]);
+						int t1_h = Integer.parseInt(t1.split(":")[0]);
+						int t1_m = Integer.parseInt(t1.split(":")[1]);
+						int t1_s = Integer.parseInt(t1.split(":")[2]);
+						int t1_f = Integer.parseInt(t1.split(":")[3]);
+						
+						
+						int r_s;
+						int r_m;
+						int r_h;
+						
+						t0_s = t0_f > 15 ? t0_s + 1 : t0_s;
+						t1_s = t1_f > 15 ? t1_s + 1 : t1_s;
+						
+						if (t0_s <= t1_s) {
+							r_s = t1_s - t0_s;
+						}
+						else {
+							r_s = t1_s + 60 - t0_s;
+							t0_m ++;
+						}
+						
+						if (t0_m <= t1_m) {
+							r_m = t1_m - t0_m;
+						}
+						else {
+							r_m = t1_m + 60 - t0_m;
+							t0_h ++;
+						}
+						r_h = t1_h - t0_h;
+						
+						temps_in = String.format("%02d:%02d:%02d", t0_h, t0_m, t0_s);
+						temps_out = String.format("%02d:%02d:%02d", t1_h, t1_m, t1_s);
+						temps_diff = r_h == 0 ? String.format("%d:%02d", r_m, r_s) : String.format("%d:%02d:%02d",r_h, r_m, r_s);
+						
 					}
-					else {
-						r_m = t1_m + 60 - t0_m;
-						t0_h ++;
-					}
-					r_h = t1_h - t0_h;
 					
-					temps_in = String.format("%02d:%02d:%02d", t0_h, t0_m, t0_s);
-					temps_out = String.format("%02d:%02d:%02d", t1_h, t1_m, t1_s);
-					temps_diff = r_h == 0 ? String.format("%d:%02d", r_m, r_s) : String.format("%d:%02d:%02d",r_h, r_m, r_s);
-	
 				}	
-				//bw.write(currentLine + ls);
 				
 			}
 			bw.close();
@@ -205,6 +246,12 @@ public class UCS_CSV {
 	}
 	
 	public static void surfer(String ref_ttl){
+		
+		if (ref_ttl.contains("'")){
+			System.out.println("modification ...");
+			ref_ttl = ref_ttl.replaceAll("'", "\\\\\\" + "'");
+			System.out.println("titre  : " + ref_ttl);
+		}
 		
 		id = null;
 		link = null;
@@ -219,19 +266,24 @@ public class UCS_CSV {
 			response = client.execute(request);
 			
 			
-			System.out.println(response.toString());
+			//System.out.println(response.toString());
 			
 			rd = new BufferedReader(
 					new InputStreamReader(response.getEntity().getContent()));
 
 			    String line = "";
 				while ((line = rd.readLine()) != null) {
-					if (! flag_test2 && line.contains(ref_ttl)){
-						id = line.split("soundPreview")[1].substring(3, 8);
+					if (! flag_test2 && line.split("play").length > 2 && line.split("play")[2].toLowerCase().contains(ref_ttl.toLowerCase())){
+
+						//System.out.println("---> " +  line.split("soundPreview")[1].split(",")[1]);
+						
+						id = line.split("soundPreview")[1].split(",")[1];
 						flag_test2 = true;
 					}
 					else if (flag_test2 && line.contains(String.format("%s.aspx", id))){
 						link = line.split("\"")[1];
+						
+						System.out.println("LINK ---> " + link);
 						flag_test2 = false;
 						break;
 						
@@ -262,7 +314,7 @@ public class UCS_CSV {
 		    line = "";
 			while ((line = rd.readLine()) != null) {
 				
-				if (line.contains(ref_title_push_save)){
+				if (line.toLowerCase().contains(ref_title_push_save.toLowerCase())){
 					flag_secteur2 = true;
 				}
 				
@@ -285,7 +337,7 @@ public class UCS_CSV {
 			}
 
 			
-			System.out.println(isrc + "\n");
+			System.out.println("ISRC ---> " + isrc + "\n");
 		}catch ( IOException e) {
 			// TODO Bloc catch généré automatiquement
 
@@ -297,7 +349,7 @@ public class UCS_CSV {
 		//ref_alt = ref.split("/")[1].substring(0,1);
 		ref_title_push = Arrays.asList(ref_title_push.split(" ")).subList(0, ref_title_push.split(" ").length -1).stream().collect(Collectors.joining(" "));
 		//surfer(String.format("%s/%s", ref.split("/")[0], ref_alt));
-		System.out.println("alt : " + ref_title_push);
+		//System.out.println("alt : " + ref_title_push);
 		surfer(ref_title_push);
 
 	}
